c028127a2ce9f4e7eee27cbed0bbea0b70c8646d
Akiyoshi Honda <b8_q6ja3@yahoo.co.jp>
2026-03-09 21:08:07 +0900

    Add typed commit probe helper

---
+++ foxar/script/CoreCatsDirectCommitProbe.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^1.1.2;
+
+import "spark-std/Script.sol";
+import "../src/CoreCats.sol";
+
+contract CoreCatsDirectCommitProbeScript is Script {
+    function run() external {
+        address coreCatsAddress = vm.envAddress("CORECATS_ADDRESS");
+        address minter = vm.envAddress("MINTER_ADDRESS");
+        uint256 warpTimestamp = vm.envOr("MINT_WARP_TIMESTAMP", uint256(0));
+        uint256 quantityValue = vm.envUint("MINT_QUANTITY");
+        bytes32 commitHash = vm.envBytes32("MINT_COMMIT_HASH");
+        uint256 nonce = vm.envUint("MINT_NONCE");
+        uint256 expiry = vm.envUint("MINT_EXPIRY");
+        bytes memory signature = vm.envBytes("MINT_SIGNATURE");
+
+        if (warpTimestamp != 0) {
+            vm.warp(warpTimestamp);
+        }
+
+        vm.prank(minter);
+        CoreCats(coreCatsAddress).commitMint(uint8(quantityValue), commitHash, nonce, expiry, signature);
+    }
+}
