c861e690e64bb8be50f96a45085b5e7e5995bbd3
Akiyoshi Honda <b8_q6ja3@yahoo.co.jp>
2026-03-13 19:34:03 +0900

    Adjust teaser mobile transparency and curated grids

--- web-public-teaser/app/page.js
+++ web-public-teaser/app/page.js
@@ -4,8 +4,8 @@ import { getCollection, getSummary } from "../../web/lib/viewer-data";
 import { getCorePublicConfig } from "../../web/lib/server/core-env";
 import { isTeaserDisplayEnabled } from "../../web/lib/server/teaser-display.js";
 
-const HOME_NATURAL_IDS = [272, 241, 690, 647, 322, 818, 515, 102, 922, 415];
-const HOME_SPECIAL_IDS = [48, 305, 714, 25, 903, 939, 479, 489, 1000, 999];
+const HOME_NATURAL_IDS = [1, 102, 241, 272, 322, 415, 515, 647, 690, 802, 818, 922];
+const HOME_SPECIAL_IDS = [25, 48, 194, 305, 479, 489, 714, 866, 903, 939, 1000, 999];
 
 function Metric({ value, label }) {
   return (
